aa59cc609d5e143b4e2f243101f131a91f6859e6
hau <nguyenhau1720@gmail.com>
2024-11-18 17:23:16 +0700

    add bt

---
+++ SV/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Student student = new Student();
+                student.Input();
+                student.Output();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ SV/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SV")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SV")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1fc400e2-9bf1-4222-b6df-63f9ae24eb98")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
